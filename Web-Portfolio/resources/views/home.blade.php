9c1e6ce1be093bb667a59cffb650a5f41d1cb0d1
foxxxesky <abdurrahman1467@gmail.com>
2022-01-24 07:02:08 +0700

    profile section

---
+++ Web-Portfolio/resources/views/home.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Syariif Abd | Web Portfolio</title>
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- nav -->
+    <div class="container pt-4">
+        <div class="row justify-content-between">
+            <div class="col-md-11">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Profile -->
+    <section>
+        <div class="container pt-4">
+            <div class="row justify-content-between">
+                <div class="col-md-6">
+                    <p id="profile-hello">Hello There 👋 I’m</p>
+                    <h4 id="profile-name">Syariif Abdurrahman</h4>
+                    <h4 id="profile-name">Bathik</h4>
+                    <hr width="144px" size="4px" id="line">
+                    <p class="pt-3" id="profile-desc">I am an undergraduate student in Information Systems at the
+                        Faculty of
+                        Industrial Engineering,
+                        Telkom University who wants to have a career in IT and Business. I am an enthusiastic student
+                        and have a strong motivation in facing challenges and exploring new things. I also have good
+                        communication, am conscientious, can work in a team, and can adapt to a new work environment. I
+                        am currently looking for opportunities and opportunities to continue to progress and develop.
+                    </p>
+                    <div class="pt-3">
+                        <form action="" method="post">
+                            <tr>
+                                <td>
+                                    <button class="btn" id="button-download-cv">Download CV</button>
+                                </td>
+                                <span class="ps-4"></span>
+                                <td>
+                                    <a class="btn" id="button-hire">Hire Me</a>
+                                </td>
+                            </tr>
+                        </form>
+                    </div>
+                    <div class="pt-5">
+                        <p id="profile-social">Social</p>
+                        <tr>
+                            <td><img src="images/linkedin.png" alt=""></td>
+                        </tr>
+                        <span class="ps-2"></span>
+                        <tr>
+                            <td><img src="images/github.png" alt=""></td>
+                        </tr>
+                        <span class="ps-2"></span>
+                        <tr>
+                            <td><img src="images/discord.png" alt=""></td>
+                        </tr>
+                        <span class="ps-2"></span>
+                        <tr>
+                            <td><img src="images/gmail.png" alt=""></td>
+                        </tr>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="text-end">
+                        <img src="images/ProfilePictures.png" alt="" width="518px" height="517px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
